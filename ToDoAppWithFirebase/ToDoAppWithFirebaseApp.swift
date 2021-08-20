a2bb9f6a8c676c667940c26eb4613b2cc8e2e6a2
zoniha <awaawapurin@gmail.com>
2021-08-20 13:58:10 +0900

    Initial Commit

---
+++ ToDoAppWithFirebase/ToDoAppWithFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppWithFirebaseApp.swift
+//  ToDoAppWithFirebase
+//
+//  Created by zoniha on 2021/08/20.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppWithFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
