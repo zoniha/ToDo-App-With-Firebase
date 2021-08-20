f3e9d26541c9860aa00419d420f31ef19e25e7ec
zoniha <awaawapurin@gmail.com>
2021-08-21 00:46:13 +0900

    ✨ feat: Add list view

--- ToDoAppWithFirebase/Views/HomeView.swift
+++ ToDoAppWithFirebase/Views/HomeView.swift
@@ -1,14 +1,25 @@
 import SwiftUI
 
 struct HomeView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		NavigationView {
+			List {
+				ForEach(0...10, id: \.self) { number in
+					VStack(alignment: .leading) {
+						Text("Title \(number)")
+							.font(.title)
+
+						Text("Description \(number)")
+					}
+				}
+			}
+			.navigationTitle("My ToDo")
+		}
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+	static var previews: some View {
+		HomeView()
+	}
 }
