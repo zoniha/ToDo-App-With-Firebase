777a20a7603682ced1212e7e0eebbe2b906298ac
zoniha <awaawapurin@gmail.com>
2021-08-21 11:44:26 +0900

    ✨ feat: Add task contents

--- ToDoAppWithFirebase/Views/DetailView.swift
+++ ToDoAppWithFirebase/Views/DetailView.swift
@@ -1,13 +1,19 @@
 import SwiftUI
 
 struct DetailView: View {
-    var body: some View {
-        Text("")
-    }
+	@State var title: String
+	@State var description: String
+
+	var body: some View {
+		Text(description)
+			.navigationTitle(title)
+	}
 }
 
 struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
-    }
+	static var previews: some View {
+		NavigationView {
+			DetailView(title: "Test Title", description: "Test Description")
+		}
+	}
 }
