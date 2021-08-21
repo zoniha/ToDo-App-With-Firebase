72dddc6c47842e8407b7f2df11b8995868e683cc
zoniha <awaawapurin@gmail.com>
2021-08-22 00:00:20 +0900

    ✨ feat: Set up firebase

--- ToDoAppWithFirebase/ToDoAppWithFirebaseApp.swift
+++ ToDoAppWithFirebase/ToDoAppWithFirebaseApp.swift
@@ -1,10 +1,21 @@
 import SwiftUI
+import Firebase
 
 @main
 struct ToDoAppWithFirebaseApp: App {
-    var body: some Scene {
+	@UIApplicationDelegateAdaptor(Delegate.self) var delegate
+
+	var body: some Scene {
         WindowGroup {
             HomeView()
         }
     }
+}
+
+final class Delegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+
+		return true
+	}
 }
