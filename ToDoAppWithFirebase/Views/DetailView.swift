59382439fc02443d3994f2f8ce780438190d6552
zoniha <awaawapurin@gmail.com>
2021-08-21 11:26:08 +0900

    🎉 init: Initial commit

---
+++ ToDoAppWithFirebase/Views/DetailView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
