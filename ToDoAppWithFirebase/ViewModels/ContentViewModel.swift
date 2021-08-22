8dd7d1e79fe766130882a49847368dbc8d8713e9
zoniha <awaawapurin@gmail.com>
2021-08-22 11:34:03 +0900

    ✨ feat: Add loading todo items

--- ToDoAppWithFirebase/ViewModels/ContentViewModel.swift
+++ ToDoAppWithFirebase/ViewModels/ContentViewModel.swift
@@ -1,1 +1,23 @@
-import Foundation
+import SwiftUI
+import Combine
+
+final class ContentViewModel: ObservableObject {
+	@Published var items = [TODOItem]()
+	private var cancellables = Set<AnyCancellable>()
+
+	func loadItems() {
+		FirebaseService.fetchTODOs()
+			.sink { (completion) in
+				switch completion {
+				case .failure(let error):
+					print(error)
+					return
+				case .finished:
+					return
+				}
+			} receiveValue: { [weak self] (items) in
+				self?.items = items
+			}
+			.store(in: &cancellables)
+	}
+}
