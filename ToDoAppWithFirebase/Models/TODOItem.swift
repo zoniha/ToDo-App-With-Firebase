3c969909c26e1df456eaf37b83cc2860e69e34f6
zoniha <awaawapurin@gmail.com>
2021-08-22 11:33:12 +0900

    ✨ feat: Add todo item model

--- ToDoAppWithFirebase/Models/TODOItem.swift
+++ ToDoAppWithFirebase/Models/TODOItem.swift
@@ -1,7 +1,22 @@
 import Foundation
 
-struct TODOItem {
+struct TODOItem: Decodable {
 	var title: String
 	var description: String
+
+	init(from decoder: Decoder) {
+		do {
+			let values = try decoder.container(keyedBy: TODOItemKeys.self)
+			self.title = try values.decode(String.self, forKey: .title)
+			self.description = try values.decode(String.self, forKey: .description)
+		} catch {
+			self.title = "Title error"
+			self.description = "Desc error"
+		}
+	}
 }
 
+enum TODOItemKeys: String, CodingKey {
+	case title
+	case description
+}
