8cfa7d0ad0294d41147d374204b4714c213c86cb
zoniha <awaawapurin@gmail.com>
2021-08-22 11:31:15 +0900

    ✨ feat: Add fetch todo data

--- ToDoAppWithFirebase/Services/FirebaseService.swift
+++ ToDoAppWithFirebase/Services/FirebaseService.swift
@@ -1,1 +1,34 @@
-import Foundation
+import Combine
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct FirebaseService {
+	static let db = Firestore.firestore()
+
+	static func fetchTODOs() -> AnyPublisher<[TODOItem], Error> {
+		Future<[TODOItem], Error> { promise in
+			self.db.collection("todos")
+				.getDocuments { (snapshot, error) in
+					if let error = error {
+						promise(.failure(error))
+						return
+					}
+
+					guard let snapshot = snapshot else {
+						promise(.failure(FirebaseError.badSnapShot))
+						return
+					}
+
+					var items = [TODOItem]()
+					snapshot.documents.forEach { document in
+						if let item = try? document.data(as: TODOItem.self) {
+							if items.contains(where: { $0.title == item.title }) { return }
+							items.append(item)
+						}
+					}
+					promise(.success(items))
+				}
+		}
+		.eraseToAnyPublisher()
+	}
+}
