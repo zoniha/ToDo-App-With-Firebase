20dc8958cb18294dde9eed1e6aa099f51476fef1
zoniha <awaawapurin@gmail.com>
2021-08-22 16:44:36 +0900

    ✨ feat: Add create func

--- ToDoAppWithFirebase/ViewModels/AddItemViewModel.swift
+++ ToDoAppWithFirebase/ViewModels/AddItemViewModel.swift
@@ -1,1 +1,24 @@
-import Foundation
+import SwiftUI
+import Combine
+
+final class AddItemViewModel: ObservableObject {
+	@Published var title = ""
+	@Published var description = ""
+
+	private var cancellables = Set<AnyCancellable>()
+
+	func create() {
+		let item = TODOItem(title: title, description: description)
+		FirebaseService.create(item)
+			.sink { (completion) in
+				switch completion {
+				case .failure(let error):
+					print(error)
+					return
+				case .finished:
+					return
+				}
+			} receiveValue: { _ in }
+			.store(in: &cancellables)
+	}
+}
